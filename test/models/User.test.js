d26ff65ba5bdd10e6d0a3d9a3d44687b5893cd9c
KelvinLeeder <kleeder77@outlook.com>
2022-05-11 23:23:09 -0500

    Modificacion de datos para prueba test

---
+++ test/models/User.test.js
@@ -0,0 +1,19 @@
+const User = require('./../../app/models/User')
+
+describe("Unit Tests for User class", () =>{
+
+    test('Creat an User Object', () =>{
+        //Aqui invocamos el codigo que vamos a usar en la app 
+        const user = new User(1, "KLeeder_07", "Kelvin", "Bio", "dateCreated", "lastUpdate" )
+
+        //Aqui se valida los resultados de ese codigo
+        //Esta es una comparacion que va a igualar el valor de la izquiera con el con el valor de la derecha(Valor esperado)
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("KLeeder_07")
+        expect(user.name).toBe("Kelvin")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        xpect(user.lastUpdate).toBe("lastUpdate")
+        
+    });
+})
