c12ca868d93296ed9f3552a26ed84ed9d00bc164
KelvinLeeder <kleeder77@outlook.com>
2022-05-13 22:08:03 -0500

    Se agrega una prueba de requerimientos para el nuevo usuario

---
+++ test/services/UserService.test.js
@@ -0,0 +1,13 @@
+const UserService = require('./../../app/services/UserService')
+
+describe("Test for UserService", () => {
+
+    test("1. Create a new user using the UserService", () =>{
+        const user = UserService.create(1, "KLeeder_07", "Kelvin")
+        expect(user.username).toBe("KLeeder_07")
+        expect(user.name).toBe("Kelvin")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeundefined()
+
+    })
+})
